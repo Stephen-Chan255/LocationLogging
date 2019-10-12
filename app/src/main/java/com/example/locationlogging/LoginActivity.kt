be6eefdc9ff524a68ac9c91eeca7e1f558874e1f
guoqing.chen <stephen.chan255@gmail.com>
2019-10-12 20:18:43 +1000

    add new MapsActivity

--- app/src/main/java/com/example/locationlogging/LoginActivity.kt
+++ app/src/main/java/com/example/locationlogging/LoginActivity.kt
@@ -1,12 +1,18 @@
 package com.example.locationlogging
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        text_login_anonymous.setOnClickListener {
+            startActivity(Intent(this, MapsActivity::class.java))
+        }
     }
 }
